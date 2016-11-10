aeeaf4df5f905d54dcf98f1ab2613da6dbbdd4e5
zhangwenjun <zhangwenjun0630@163.com>
2016-11-10 18:06:41 +0800

    本地和网络都可播放，进度条需改进，全屏未完成

---
+++ BJPlayer/BJPlayer/Class/Config/GlobalData.h
@@ -0,0 +1,13 @@
+//
+//  GlobalData.h
+//  BJPlayer
+//
+//  Created by zhangwenjun on 16/11/10.
+//  Copyright © 2016年 zhangwenjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GlobalData : NSObject
+
+@end
---
+++ BJPlayer/BJPlayer/Class/Config/GlobalData.m
@@ -0,0 +1,13 @@
+//
+//  GlobalData.m
+//  BJPlayer
+//
+//  Created by zhangwenjun on 16/11/10.
+//  Copyright © 2016年 zhangwenjun. All rights reserved.
+//
+
+#import "GlobalData.h"
+
+@implementation GlobalData
+
+@end

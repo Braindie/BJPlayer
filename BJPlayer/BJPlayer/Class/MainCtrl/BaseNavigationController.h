07f73069b5c9ec2a62a47062e19ed53dbb53e17f
zhangwenjun <zhangwenjun0630@163.com>
2016-11-14 18:10:27 +0800

    优化屏幕点击滑动交互

---
+++ BJPlayer/BJPlayer/Class/MainCtrl/BaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationController.h
+//  BJPlayer
+//
+//  Created by zhangwenjun on 16/11/14.
+//  Copyright © 2016年 zhangwenjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNavigationController : UINavigationController
+
+@end

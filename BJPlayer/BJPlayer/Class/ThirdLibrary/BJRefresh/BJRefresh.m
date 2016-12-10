aa2ce8571a4fad6d7da5edf022371b35203dfbe1
zhangwenjun <zhangwenjun0630@163.com>
2016-12-10 17:55:56 +0800

    刷新动画,未完成

---
+++ BJPlayer/BJPlayer/Class/ThirdLibrary/BJRefresh/BJRefresh.m
@@ -0,0 +1,21 @@
+//
+//  BJRefresh.m
+//  BJPlayer
+//
+//  Created by zhangwenjun on 16/12/10.
+//  Copyright © 2016年 zhangwenjun. All rights reserved.
+//
+
+#import "BJRefresh.h"
+
+@implementation BJRefresh
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

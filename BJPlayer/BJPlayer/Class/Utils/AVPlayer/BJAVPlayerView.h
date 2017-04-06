87f62f4e61e3a581f4e4173c30fc1dbce6114879
zhangwenjun <zhangwenjun0630@163.com>
2017-04-06 17:34:30 +0800

    AVPlayer播放

---
+++ BJPlayer/BJPlayer/Class/Utils/AVPlayer/BJAVPlayerView.h
@@ -0,0 +1,13 @@
+//
+//  BJAVPlayerView.h
+//  BJPlayer
+//
+//  Created by zhangwenjun on 17/4/6.
+//  Copyright © 2017年 zhangwenjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BJAVPlayerView : UIView
+
+@end
---
+++ BJPlayer/BJPlayer/Class/Utils/AVPlayer/BJAVPlayerView.m
@@ -0,0 +1,21 @@
+//
+//  BJAVPlayerView.m
+//  BJPlayer
+//
+//  Created by zhangwenjun on 17/4/6.
+//  Copyright © 2017年 zhangwenjun. All rights reserved.
+//
+
+#import "BJAVPlayerView.h"
+
+@implementation BJAVPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
